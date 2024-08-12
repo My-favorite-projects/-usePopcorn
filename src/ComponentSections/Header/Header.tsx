d2eb8f822a17ad37ff51ab7594f9bd4169ce12ab
mehrdadj2005 <mehrdadjokari13@gmail.com>
2024-08-12 13:43:58 +0330

    # create header component

--- src/ComponentSections/Header/Header.tsx
+++ src/ComponentSections/Header/Header.tsx
@@ -1,3 +1,57 @@
-export default function Header(params: any) {
-  return <></>;
+import { Box, Typography, IconButton, InputBase, Button } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+// interface Param {
+//   bg: string;
+// }
+
+export default function Header(props: any) {
+  console.log(props.bg);
+
+  return (
+    <>
+      <Box
+        className={`flex justify-between py-3 bg-${props.bg ? props.bg : null}`}
+      >
+        <Box className="flex items-center">
+          <img src="./public/images/tv.png" alt="Logo" className="mr-2" />
+          <Button>
+            <Typography
+              variant="h4"
+              className={`ffb ${props.bg ? "text-light" : "text-dark"}`}
+            >
+              MovieBox
+            </Typography>
+          </Button>
+        </Box>
+
+        <Box className="border border-2 rounded-md w-2/5 flex items-center">
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="What do you want to watch?"
+            inputProps={{ "aria-label": "What do you want to watch?" }}
+            className="w-full ffb text-8xl"
+          />
+          <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Box>
+
+        <Box className="flex items-center">
+          <Button
+            variant="text"
+            size="small"
+            sx={{ border: 2, borderColor: props.bg ? "white" : "black" }}
+          >
+            <Typography
+              variant="h5"
+              className={`ffb ${props.bg ? "text-secondary" : "text-dark"}`}
+            >
+              Movie List
+            </Typography>
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
 }
