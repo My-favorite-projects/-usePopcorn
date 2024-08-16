72894c319ff44446bbaf02830af0be183137b93b
Mahsa <arabzadehmah@gmail.com>
2024-08-16 20:46:20 +0430

    Add(card): add cardMedia and set data

--- src/Components/ComponentsElement/Card.tsx
+++ src/Components/ComponentsElement/Card.tsx
@@ -1,4 +1,5 @@
-import { Box } from "@mui/material";
+import CardMedia from "@mui/material/CardMedia";
+import { Box, Typography } from "@mui/material";
 
 export default function Card() {
   return (
@@ -9,7 +10,18 @@ export default function Card() {
           height: 490,
           bgcolor: "#fff",
         }}
-      />
+      >
+        <CardMedia
+          component="img"
+          height="370"
+          width="250"
+          image="./public/images/Poster.svg"
+          alt="stranger things"
+        />
+        <Typography variant="span" component="h">
+          USA, 2016 - Current
+        </Typography>
+      </Box>
     </div>
   );
 }
