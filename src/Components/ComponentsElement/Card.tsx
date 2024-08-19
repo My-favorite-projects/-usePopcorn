a59a9d5a6de1e15c3661750d5d5ee3517e0e4579
Mahsa <arabzadehmah@gmail.com>
2024-08-19 11:56:45 +0430

    Refactor(Card): Clear the extra div in the card component

--- src/Components/ComponentsElement/Card.tsx
+++ src/Components/ComponentsElement/Card.tsx
@@ -4,43 +4,41 @@ import IMDb from "./ScoreMovie";
 
 export default function Card() {
   return (
-    <div>
-      <Box
-        className="text-secondary"
-        sx={{
-          width: "100%",
-          height: "100%",
-          bgcolor: "#fff",
-        }}
+    <Box
+      className="text-secondary"
+      sx={{
+        width: "100%",
+        height: "100%",
+        bgcolor: "#fff",
+      }}
+    >
+      <CardMedia
+        component="img"
+        height="370"
+        image="./public/images/Poster.svg"
+        alt="stranger things"
+      />
+      <Typography sx={{ fontSize: 12, fontWeight: 600 }} variant="subtitle1">
+        USA, 2016 - Current
+      </Typography>
+      <Typography
+        className="text-dark"
+        sx={{ fontSize: 18, fontWeight: 600 }}
+        variant="h5"
+        component="text"
       >
-        <CardMedia
-          component="img"
-          height="370"
-          image="./public/images/Poster.svg"
-          alt="stranger things"
-        />
-        <Typography sx={{ fontSize: 12, fontWeight: 600 }} variant="subtitle1">
-          USA, 2016 - Current
-        </Typography>
-        <Typography
-          className="text-dark"
-          sx={{ fontSize: 18, fontWeight: 600 }}
-          variant="h5"
-          component="text"
-        >
-          USA, 2016 - Current
-        </Typography>
-        <IMDb
-          colorScore="dark"
-          percentage="97"
-          score="88"
-          size="12"
-          weight="800"
-        />
-        <Typography variant="h6" sx={{ fontSize: 12, fontWeight: 600 }}>
-          Action, Adventure, Horror
-        </Typography>
-      </Box>
-    </div>
+        USA, 2016 - Current
+      </Typography>
+      <IMDb
+        colorScore="dark"
+        percentage="97"
+        score="88"
+        size="12"
+        weight="800"
+      />
+      <Typography variant="h6" sx={{ fontSize: 12, fontWeight: 600 }}>
+        Action, Adventure, Horror
+      </Typography>
+    </Box>
   );
 }
