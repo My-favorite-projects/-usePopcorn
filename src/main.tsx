4c12aa4e46652496b1876cf7b31006af9d4974bb
mehrdadj2005 <mehrdadjokari13@gmail.com>
2024-08-19 14:15:40 +0330

    # Calling CardDetails component
    Desc :
    - In comment mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import LandingPage from "./Components/ComponentsPage/LandingPage/LandingPage";
 import "./main.css";
+import CardDetails from "./Components/ComponentsPage/CardDetails/CardDetails";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <LandingPage />
+    {/* <CardDetails /> */}
   </StrictMode>
 );
