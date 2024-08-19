ceda98b35cb920ec6675b5bacd6c900ea3a700ee
mehrdadj2005 <mehrdadjokari13@gmail.com>
2024-08-19 14:09:14 +0330

    add Key to icon star in component (name : StarScore )

--- src/Components/ComponentsElement/StarScore.tsx
+++ src/Components/ComponentsElement/StarScore.tsx
@@ -4,14 +4,14 @@ import StarBorderIcon from "@mui/icons-material/StarBorder";
 
 export default function StarScore(props: any) {
   return (
-    <>
+    <Box>
       {Array.from({ length: 5 }, (_, i) =>
         i < props.lengthStar ? (
           <StarIcon key={i} className="text-yellow-400" />
         ) : (
-          <StarBorderIcon className="text-yellow-400" />
+          <StarBorderIcon key={i} className="text-yellow-400" />
         )
       )}
-    </>
+    </Box>
   );
 }
