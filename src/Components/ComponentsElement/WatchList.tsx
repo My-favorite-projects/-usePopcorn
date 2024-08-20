4790c1fa0e0eddbb281f4d494ac8c833d0e2cf38
Mahsa <arabzadehmah@gmail.com>
2024-08-20 14:43:49 +0430

    Feat(WatchList): Create a variable to check whether a movie is selected or not

--- src/Components/ComponentsElement/WatchList.tsx
+++ src/Components/ComponentsElement/WatchList.tsx
@@ -1,19 +1,29 @@
 // import React, { useState } from "react";
 import { Drawer, List, ListItem, ListItemText } from "@mui/material";
 
-export default function WatchListDrawer({ isOpen, setIsOpen }) {
+export default function WatchListDrawer({
+  isOpen,
+  setIsOpen,
+  isMovieSelected = true,
+}) {
   return (
     <Drawer anchor="right" open={isOpen} onClose={() => setIsOpen(false)}>
       <List>
-        {[
-          "Attack on titan",
-          "Avengers: End game",
-          "John Wick 3: Parabellum John Wick 3: Parabellum ",
-        ].map((text) => (
-          <ListItem key={text}>
-            <ListItemText primary={text} />
+        {isMovieSelected ? (
+          <ListItem>
+            <ListItemText primary="No movie has been selected" />
           </ListItem>
-        ))}
+        ) : (
+          [
+            "Attack on titan",
+            "Avengers: End game",
+            "John Wick 3: Parabellum John Wick 3: Parabellum ",
+          ].map((text) => (
+            <ListItem key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))
+        )}
       </List>
     </Drawer>
   );
