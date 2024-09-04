b533eb31682ab2cf757c9cabe529958725dd6b91
Mahsa <arabzadehmah@gmail.com>
2024-09-04 21:16:48 +0430

    Fix(hero): fix image address in dist file

--- src/Components/ComponentSections/Hero/dataCards.tsx
+++ src/Components/ComponentSections/Hero/dataCards.tsx
@@ -2,14 +2,14 @@ import { imdbWeb } from "../../../constant/constants";
 
 export const dataCardHero = [
   {
-    img: "./public/images/Poster.png",
+    img: "./images/Poster.png",
     title: "John Wick : Parabellum",
     desc: "John Wick is on the run after killing a member of the international assassins' guild, and with a $14 million price tag on his head, he is the target of hit men and women everywhere.",
     href: `${imdbWeb}tt6146586`,
     rating: 76,
   },
   {
-    img: "./public/images/blackWidow.PNG",
+    img: "./images/blackWidow.PNG",
     title: "Black Widow 2021",
     desc: "Natasha Romanoff confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises.",
     href: `${imdbWeb}tt3480822`,
