279b95cbee509bc48a7a61cd25cd34d181ba8b3a
Mahsa <arabzadehmah@gmail.com>
2024-09-05 18:09:37 +0430

    Add apiurl to constants

--- src/constant/constants.tsx
+++ src/constant/constants.tsx
@@ -1,4 +1,4 @@
 // export const  = "6c9f2c52";
 export const imdbWeb = "https://www.imdb.com/title/";
-export const apiUrl = import.meta.env.VITE_API_URL;
-export const apiKey = import.meta.env.VITE_API_KEY;
+export const apiUrl = import.meta.env.VITE_API_URL || "http://www.omdbapi.com/";
+export const apiKey = import.meta.env.VITE_API_KEY || "6c9f2c52";
