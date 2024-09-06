05a6e9fc9c41d18aeed6f882c46ad962a90ea793
mehrdadj2005 <mehrdadjokari13@gmail.com>
2024-09-06 17:57:23 +0330

    add setting workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/-usePopcorn/',
   plugins: [react()],
+  base:'/-usePopcorn/',
+  build: {
+    outDir: 'dist'
+  },
 })
